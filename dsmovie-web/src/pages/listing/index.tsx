a74fcf488e13ac6c3b8892bbf4a74c242c3a6f91
Matheus <matheus.gorga@b2breservas.com.br>
2022-05-14 19:14:25 -0300

    props

--- dsmovie-web/src/pages/listing/index.tsx
+++ dsmovie-web/src/pages/listing/index.tsx
@@ -7,34 +7,36 @@ import { BASE_URL } from 'utils/requests';
 
 function Listing() {
   const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
-      const data = response.data as MoviePage;
-      setPageNumber(data.number);
-      console.log(data);
-    });
-  }, []);
-
-  const movie = {
-    id: 1,
-    image:
-      'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg',
-    title: 'The Witcher',
-    count: 2,
-    score: 4.5,
-  };
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
 
   return (
     <>
-      {pageNumber}
       <Pagination />
 
       <div className='container'>
         <div className='row'>
-          {[1, 2, 3, 4, 5, 6].map((e) => {
+          {page.content.map((movie) => {
             return (
-              <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
+              <div key={movie.id} className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
                 <MovieCard
                   id={movie.id}
                   image={movie.image}
